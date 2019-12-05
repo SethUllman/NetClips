a7d64fe89e0d77c7ad4e0f3c10730a625029f07c
Seth Ullman <seth.ullman98@gmail.com>
2019-12-05 14:58:51 -0700

    adjust movie ul spacing

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -5,70 +5,56 @@ import { logout } from '../../actions/session';
 
 export default ({ currentUser, logout }) => {
 
-// class NavBar extends React.Component{
 
-//   constructor(props){
-//     super(props);
-//     this.handleList = this.handleList.bind(this);
-//   }
-
-//   handleList(){
-//     debugger;
-//     this.props.history.push(`/watch_list/${currentUser}`);
-//   }
-//   render(){
-  const display = currentUser ? (
-      <div>
-        <ul className='nav-bar-ul'>
-          <li className='nav-bar-li'>
-            <Link to='/movies'>Home</Link>
-          </li>
-          <li className='nav-bar-li'>TV Shows</li>
-          <li className='nav-bar-li'>Movies</li>
-          <li className='nav-bar-li'>Recently Added</li>
-          <li className='nav-bar-li'>
-            {/* <Link to={`/watch_list/${currentUser}`}>My List</Link> */}
-            My List
-          </li>
-        </ul>
-        
-      </div>
-    ) : (
-      <div>
-        
-      </div>
-    );
-    const logoutBtn = currentUser ? (
-      <ul className='search-header'>
-        <li>
-          <label className='search-bar'>
-            <FaSearch/>
-            <input type="text"/>
-          </label>
+const display = currentUser ? (
+    <div>
+      <ul className='nav-bar-ul'>
+        <li className='nav-bar-li'>
+          <Link to='/movies'>Home</Link>
         </li>
-        <li>
-          <div className='logout-btn'>
-              <button onClick={logout}>Logout</button>
-          </div>
+        <li className='nav-bar-li'>TV Shows</li>
+        <li className='nav-bar-li'>Movies</li>
+        <li className='nav-bar-li'>Recently Added</li>
+        <li className='nav-bar-li'>
+          {/* <Link to={`/watch_list/${currentUser}`}>My List</Link> */}
+          My List
         </li>
       </ul>
-    ) : (
-      <div></div>
-    )
-    return (
-      <header className="nav-bar">
-        <div className='nav-options'>
-          <h1 className="logo">NETCLIPS</h1>
-          <div>
-            {display}
-          </div>
+      
+    </div>
+  ) : (
+    <div>
+      
+    </div>
+  );
+  const logoutBtn = currentUser ? (
+    <ul className='search-header'>
+      <li>
+        <label className='search-bar'>
+          <FaSearch/>
+          <input type="text"/>
+        </label>
+      </li>
+      <li>
+        <div className='logout-btn'>
+            <button onClick={logout}>Logout</button>
         </div>
-        <div >
-          {logoutBtn}
+      </li>
+    </ul>
+  ) : (
+    <div></div>
+  )
+  return (
+    <header className="nav-bar">
+      <div className='nav-options'>
+        <h1 className="logo">NETCLIPS</h1>
+        <div>
+          {display}
         </div>
-      </header>
-    )
-  }
-// }
-
-// export default NavBar;
+      </div>
+      <div >
+        {logoutBtn}
+      </div>
+    </header>
+  )
+}
