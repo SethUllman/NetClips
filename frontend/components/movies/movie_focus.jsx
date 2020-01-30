1088e3638f2f6e35c678a549438ea3ff07cbdc94
Seth Ullman <seth.ullman98@gmail.com>
2020-01-29 23:23:07 -0700

    refactor to use movie_focus function

--- frontend/components/movies/movie_focus.jsx
+++ frontend/components/movies/movie_focus.jsx
@@ -1,57 +1,60 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 
 const movieFocus = (currentMovie, that) => {
-  
-  return (
-  <div className='drop-content'>
-    <div className='drop-background'>
-      <div className='left'></div>
-      <div className='right'>
-        <ReactPlayer
-          className='drop-movie'
-          url={this.state.currentMovie.video_url}
-          playing={true}
-          controls={false}
-          width={'68vw'}
-          height={'42vw'}
-          loop={false}
-          volume={0}
-          muted={true}
-        />
-        <div className='right-X' onClick={() => { this.hideResults() }}>X</div>
-      </div>
-    </div>
-    <div className='drop-content-container'>
-      <div className='movie-title-div'>
-        <h1 className='movie-title'>{this.state.currentMovie.title}</h1>
-      </div>
-      <div className='movie-info-div'>
-        <h2 className='movie-year'>{this.state.currentMovie.year}</h2>
-        <h2 className='movie-rating'>{this.state.currentMovie.maturity_rating}</h2>
-      </div>
-      <div className='movie-description-div'>
-        <p className='movie-description'>{this.state.currentMovie.description}</p>
-      </div>
-      <div className='movie-buttons-div'>
-
-        <button
-          className='play-btn'
-          onClick={this.handlePlay}>PLAY</button>
-
-        {this.listButtonText()}
-      </div>
-      <div className='movie-starring-div'>
-        <div className='movie-starring'>
-          <p>Starring: </p>
+  if (currentMovie) {
+    return (
+      <div className='drop-content'>
+        <div className='drop-background'>
+          <div className='left'></div>
+          <div className='right'>
+            <ReactPlayer
+              className='drop-movie'
+              url={currentMovie.video_url}
+              playing={true}
+              controls={false}
+              width={'68vw'}
+              height={'42vw'}
+              loop={false}
+              volume={0}
+              muted={true}
+            />
+            <div className='right-X' onClick={() => { that.hideResults() }}>X</div>
+          </div>
         </div>
-        <div className='movie-cast'>
-          <p>{this.state.currentMovie.cast}</p>
+        <div className='drop-content-container'>
+          <div className='movie-title-div'>
+            <h1 className='movie-title'>{currentMovie.title}</h1>
+          </div>
+          <div className='movie-info-div'>
+            <h2 className='movie-year'>{currentMovie.year}</h2>
+            <h2 className='movie-rating'>{currentMovie.maturity_rating}</h2>
+          </div>
+          <div className='movie-description-div'>
+            <p className='movie-description'>{currentMovie.description}</p>
+          </div>
+          <div className='movie-buttons-div'>
+
+            <button
+              className='play-btn'
+              onClick={that.handlePlay}>PLAY</button>
+
+            {that.listButtonText()}
+          </div>
+          <div className='movie-starring-div'>
+            <div className='movie-starring'>
+              <p>Starring: </p>
+            </div>
+            <div className='movie-cast'>
+              <p>{currentMovie.cast}</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-   
-  );
+    );
+  } else {
+    return <div></div>
+  }
 }
 
 export default movieFocus;
--- frontend/components/movies/movie_index.jsx
+++ frontend/components/movies/movie_index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {MovieItem} from './movie_item.jsx';
+import movieFocus from './movie_focus';
 import ReactPlayer from 'react-player';
 import { FaCheck } from 'react-icons/fa';
 
@@ -119,62 +120,10 @@ class movieIndex extends React.Component{
     let sci_fi = sci_fi1.splice(0, 6);
     let drama = drama1.splice(0, 6);
     let acclaimed = acclaimed1.splice(0, 6);
-//--------------------------------------------------------
 
-    const MovieShow = (this.state.currentMovie) ? (
+    const MovieShow = movieFocus(this.state.currentMovie, this);
       
-      <div className='drop-content'>
-        <div className='drop-background'>
-          <div className='left'></div>
-          <div className='right'>
-            <ReactPlayer
-              className='drop-movie'
-              url={this.state.currentMovie.video_url}
-              playing={true}
-              controls={false}
-              width={'68vw'}
-              height={'42vw'}
-              loop={false}
-              volume={0}
-              muted={true}
-            />
-            <div className='right-X' onClick={() => {this.hideResults()}}>X</div>
-          </div>
-        </div>
-        <div className='drop-content-container'>
-          <div className='movie-title-div'>
-            <h1 className='movie-title'>{this.state.currentMovie.title}</h1>
-          </div>
-          <div className='movie-info-div'>
-            <h2 className='movie-year'>{this.state.currentMovie.year}</h2>
-            <h2 className='movie-rating'>{this.state.currentMovie.maturity_rating}</h2>
-          </div>
-          <div className='movie-description-div'>
-            <p className='movie-description'>{this.state.currentMovie.description}</p>
-          </div>
-          <div className='movie-buttons-div'>
-
-            <button 
-              className='play-btn'
-              onClick={this.handlePlay}>PLAY</button>
-
-            {this.listButtonText()}
-          </div>
-          <div className='movie-starring-div'>
-            <div className='movie-starring'>
-              <p>Starring: </p>
-            </div>
-            <div className='movie-cast'>
-              <p>{this.state.currentMovie.cast}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    ) : (
-      <div></div>
-    );
-
-    //-----------------------------------------------------------
+      
     if (this.state.currentMovie) {
       if (this.state.currentMovie.genres === ('TV Comedy')) {
         this.MovieShow1 = MovieShow;
