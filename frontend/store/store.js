ab7efe0a5a4bf5535f4cc9f9e6e54fc98d66baa5
Seth Ullman <seth.ullman98@gmail.com>
2020-02-02 01:52:14 -0700

    update package.json

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root';
