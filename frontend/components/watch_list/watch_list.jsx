f170e6a661c237237b6dcbd49a4d027360eb4982
Seth Ullman <seth.ullman98@gmail.com>
2020-02-03 14:30:20 -0700

    update watchlist styling

--- frontend/components/watch_list/watch_list.jsx
+++ frontend/components/watch_list/watch_list.jsx
@@ -17,6 +17,7 @@ class WatchList extends React.Component {
       movieList.push(
         
           <MovieItem
+            className='watch-list-item'
             key={value.id}
             movie={value}
             that={this}
@@ -25,12 +26,44 @@ class WatchList extends React.Component {
         
       )
     }  
+
+    let list1 = movieList.splice(0, 6);
+    let list2 = movieList.splice(0, 6);
+    let list3 = movieList.splice(0, 6);
+    let list4 = movieList.splice(0, 6);
+    let list5 = movieList.splice(0, 6);
+    let list6 = movieList.splice(0, 6);
     return (
       <div className='watchList'>
         <div className='watch-movies'>
           <ul className='movie-ul'>
-            <div className='movie-lis'>
-              {movieList}
+            <div className='watch-lis'>
+              {list1}
+            </div>
+          </ul>
+          <ul className='movie-ul'>
+            <div className='watch-lis'>
+              {list2}
+            </div>
+          </ul>
+          <ul className='movie-ul'>
+            <div className='watch-lis'>
+              {list3}
+            </div>
+          </ul>
+          <ul className='movie-ul'>
+            <div className='watch-lis'>
+              {list4}
+            </div>
+          </ul>
+          <ul className='movie-ul'>
+            <div className='watch-lis'>
+              {list5}
+            </div>
+          </ul>
+          <ul className='movie-ul'>
+            <div className='watch-lis'>
+              {list6}
             </div>
           </ul>
         </div>
