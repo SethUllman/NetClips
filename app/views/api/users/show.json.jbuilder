3fbd484603bd99105202ec10593f786fdbe9ae6a
Seth Ullman <seth.ullman98@gmail.com>
2020-02-02 23:53:11 -0700

    fix login function

--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -1,8 +1,4 @@
-# json.extract! @user
-# , :id, :email, :@user.movies
-
 json.currentUser do
   json.email @user.email
   json.password @user.password
-  json.movies @user.movies
 end
--- frontend/reducers/session.js
+++ frontend/reducers/session.js
@@ -6,10 +6,9 @@ const _nullSession = {
 
 export default (state = _nullSession, action) => {
   Object.freeze(state);
-  
   switch(action.type){
     case RECEIVE_CURRENT_USER:
-      return Object.assign({}, { currentUser: action.usercurrentUser });
+      return Object.assign({}, { currentUser: action.user.currentUser });
     case LOGOUT_CURRENT_USER:
       return _nullSession;
     default:
