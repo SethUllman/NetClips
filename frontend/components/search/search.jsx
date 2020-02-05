0a00f2b3324ec2ba3b33d647adbab874d04b076c
Seth Ullman <seth.ullman98@gmail.com>
2020-02-05 00:52:32 -0700

    add funtions for movie item and focus

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { MovieItem } from '../movies/movie_item';
+import movieFocus from '../movies/movie_focus';
+import { FaCheck } from 'react-icons/fa';
 
 class Search extends React.Component{
   constructor(props){
@@ -47,7 +50,98 @@ class Search extends React.Component{
     console.log(people);
   }
 
+  handlePlay(e) {
+    e.preventDefault();
+    const movie = this.state.currentMovie;
+    this.props.history.push(`/movies/${movie.id}`);
+  }
+
+  listButtonText() {
+    let found = false;
+    let i = 0;
+    while (!found && i < 51) {
+      let list = this.props.state.watchList[i];
+      if (list && (list.title === this.state.currentMovie.title)) {
+        found = true;
+      }
+      i++
+    }
+    if (found) {
+      return <button className='in-list' onClick={() => { this.updateList() }}><FaCheck />MY LIST</button>
+    } else {
+      return <button className='add-list' onClick={() => { this.updateList() }}>+ MY LIST</button>;
+    }
+  }
+
+  updateList() {
+    let found = false;
+    let i = 0;
+
+    let current = this.state.currentMovie;
+    while (!found && i < this.props.state.watchList.length) {
+      let list = this.props.state.watchList[i];
+      if (list.title === current.title) {
+        found = true;
+        this.props.deleteWatchList(current.id);
+      }
+      i++
+    }
+    if (!found) {
+
+      this.props.addWatchList(current);
+    }
+
+
+  }
+
+  placeFocus(list) {
+    let found = false;
+    list.forEach((movie) => {
+      if (movie.props.movie === this.state.currentMovie) {
+        found = true;
+      }
+    });
+    if (found) {
+      return movieFocus(this.state.currentMovie, this);
+    }
+  }
+
+  hideResults() {
+    this.setState({ currentMovie: null });
+  }
+
+  listPlacement(list) {
+    if (list.length > 0) {
+      return list;
+    } else {
+      return <div></div>
+    }
+  }
+
   render(){
+
+    const watchList = this.props.state.watchList;
+    const movieList = [];
+    for (let [key, value] of Object.entries(watchList)) {
+      movieList.push(
+
+        <MovieItem
+          key={value.id}
+          movie={value}
+          that={this}
+          state={this.state}
+        />
+
+      )
+    }
+
+    let list1 = movieList.splice(0, 6);
+    let list2 = movieList.splice(0, 6);
+    let list3 = movieList.splice(0, 6);
+    let list4 = movieList.splice(0, 6);
+    let list5 = movieList.splice(0, 6);
+    let list6 = movieList.splice(0, 6);
+
     return (
       <div className='search-results'>
         {this.searchResults()}
