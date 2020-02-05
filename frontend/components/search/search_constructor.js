d9919da02625b60f385d50504dac3301b3551ab6
Seth Ullman <seth.ullman98@gmail.com>
2020-02-04 17:07:51 -0700

    add content to search_constructor

---
+++ frontend/components/search/search_constructor.js
@@ -0,0 +1,26 @@
+import { connect } from 'react-redux';
+import Search from './search';
+import { fetchMovies, fetchMovie } from '../../actions/movie_actions';
+import { addWatchList, deleteWatchList, fetchWatchList } from '../../actions/watch_list_actions';
+import { withRouter } from 'react-router-dom';
+
+import React from 'react';
+
+const mapStateToProps = state => {
+  return {
+    movies: Object.values(state.movies),
+    watchList: Object.values(state.watchList)
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchMovies: () => dispatch(fetchMovies()),
+    fetchMovie: movie => dispatch(fetchMovie(movie)),
+    fetchWatchList: () => dispatch(fetchWatchList()),
+    addWatchList: movie => dispatch(addWatchList(movie)),
+    deleteWatchList: movieId => dispatch(deleteWatchList(movieId))
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(search));
