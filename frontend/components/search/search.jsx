6d2b9e456008d62ef8257a0a9815cfd889d4918d
Seth Ullman <seth.ullman98@gmail.com>
2020-02-05 00:35:54 -0700

    update search component to filter title results

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -3,12 +3,51 @@ import React from 'react';
 class Search extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      search: ""
+    }
+
+    this.searchResults = this.searchResults.bind(this);
+    this.filteredResults = this.filteredResults.bind(this);
+    this.checkSearch = this.checkSearch.bind(this);
+  }
+
+  componentDidMount(){
+    this.props.fetchMovies();
+    this.props.fetchWatchList();
+    this.checkSearch();
+
+  }
+
+  checkSearch(){
+    const searchCheck = setInterval( () => {
+      if ( this.state.search != window.search ){
+        this.setState({search: window.search});
+      }
+    }, 100);
+  }
+
+  searchResults() {
+    let results = [];
+    for (let i = 0; i <= 25; i++){
+      let current = this.props.movies[i];
+      if (current && current.title.includes(this.state.search)){
+        results.push(current);
+      }
+    }
+    this.filteredResults(results);
+  }
+
+  filteredResults(results) {
+    console.log(results);
   }
 
   render(){
-    return (<div>
-      Search Component
-    </div>)
+    return (
+      <div className='search-results'>
+        {this.searchResults()}
+      </div>
+    )
   }
 }
 
