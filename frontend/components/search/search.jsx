5b0b7f2ca95c716161b97bcf4c3375d35b198dff
Seth Ullman <seth.ullman98@gmail.com>
2020-02-04 17:05:46 -0700

    add search component skeleton

---
+++ frontend/components/search/search.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Search extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    <div>
+      Search Component
+    </div>
+  }
+}
+
+export default Search;
