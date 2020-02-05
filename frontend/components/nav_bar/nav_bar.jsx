ea91136792fc008ba96fc81ae91c485ff498fc3c
Seth Ullman <seth.ullman98@gmail.com>
2020-02-04 22:01:59 -0700

    add searchBar to nav_bar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -5,25 +5,32 @@ import { FaSearch } from 'react-icons/fa';
 export default ({ currentUser, logout }) => {
 
 
-const display = currentUser ? (
-    <div>
-      <ul className='nav-bar-ul'>
-        <li className='nav-bar-li'>
-          <Link to='/movies'>Home</Link>
-        </li>
-        {/* <li className='nav-bar-li'>TV Shows</li>
-        <li className='nav-bar-li'>Movies</li> */}
-        <li className='nav-bar-li'>
-          <Link to={`/watch_list/${currentUser.id}`}>My List</Link>
-        </li>
-      </ul>
-      
-    </div>
-  ) : (
-    <div>
-      
-    </div>
+  const display = currentUser ? (
+      <div>
+        <ul className='nav-bar-ul'>
+          <li className='nav-bar-li'>
+            <Link to='/movies'>Home</Link>
+          </li>
+          {/* <li className='nav-bar-li'>TV Shows</li>
+          <li className='nav-bar-li'>Movies</li> */}
+          <li className='nav-bar-li'>
+            <Link to={`/watch_list/${currentUser.id}`}>My List</Link>
+          </li>
+        </ul>
+        
+      </div>
+    ) : (
+      <div>
+        
+      </div>
   );
+
+  const searchBar = currentUser ? (
+    <div></div>
+  ) : (
+    <div></div>
+  );
+  
   const logoutBtn = currentUser ? (
     <ul className='search-header'>
       <li>
@@ -49,7 +56,8 @@ const display = currentUser ? (
     </ul>
   ) : (
     <div></div>
-  )
+  );
+
   return (
     <header className="nav-bar">
       <div className='nav-options'>
